f5e97bbf7cb9df8ffb9175edfa31b3ad38eef1f6
pachuau10 <duhteapachuau10@gmail.com>
2025-08-10 09:13:47 +0530

    Add collected static files

--- hla/admin.py
+++ hla/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
+from .models import Data, Req
 
-# Register your models here.
-from django.contrib import admin
-from . models import Data
-from . models import Req
+# Create a custom ModelAdmin for the Data model
+@admin.register(Data)
+class DataAdmin(admin.ModelAdmin):
+    # This tuple specifies which fields to display in the list view of the admin.
+    # It makes the list page more informative and easier to browse.
+    list_display = ('name', 'phuahtu', 'satu')
 
-# Register your models here.
+    # This tuple adds a search box to the admin page.
+    # The search functionality will query these fields, making it easy to find specific entries.
+    search_fields = ('name', 'phuahtu', 'satu')
 
-admin.site.register(Data)
-admin.site.register(Req)
+    # This adds a right-hand sidebar to filter the change list page.
+    # We'll filter by the 'phuahtu' and 'satu' fields.
+    list_filter = ('phuahtu', 'satu')
+
+
+# Create a custom ModelAdmin for the Req model
+@admin.register(Req)
+class ReqAdmin(admin.ModelAdmin):
+    # This will display the full text of the request in the admin list view.
+    list_display = ('req',)
+    
+    # This adds a search box to the admin page that searches the 'req' field.
+    search_fields = ('req',)
+
--- projlyrics_new/settings.py
+++ projlyrics_new/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
@@ -137,7 +138,7 @@ else:
     
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
